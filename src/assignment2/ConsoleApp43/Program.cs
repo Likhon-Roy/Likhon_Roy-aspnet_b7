860ad1e7af96ace38ce070616280e7a31ae911d1
LikhonR <likhon@learnteq.com>
2022-12-30 02:49:19 +0530

    2 done

---
+++ src/assignment2/ConsoleApp43/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+double x;
+ulong a;
+
+for (int i = 0; i < n; i++)
+{
+    a = 0;
+
+    x = double.Parse(Console.ReadLine());
+
+    var z = ((x / 2) - 1);
+
+    a = (ulong)Math.Ceiling(z);
+
+    Console.WriteLine(a);
+
+}
