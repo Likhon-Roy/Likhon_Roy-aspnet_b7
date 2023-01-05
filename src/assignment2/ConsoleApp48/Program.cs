209f3f1a9a09e8fcaf31ccd8bd9dcfb0550cfbd4
LikhonR <likhon@learnteq.com>
2023-01-05 11:11:17 +0530

    done

---
+++ src/assignment2/ConsoleApp48/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int a = 0;
+int b = 0;
+
+for (int i = 0; i < n; i++)
+{
+    var inp = Console.ReadLine().Split(' ');
+
+    foreach (string s in inp)
+    {
+        int x = int.Parse(s);
+
+        if (x > 0)
+        {
+            b += x;
+        }
+        else
+        {
+            if (b < 1)
+            {
+                ++a;
+            }
+            else
+            {
+                --b;
+            }
+        }
+    }
+    Console.WriteLine(a);
+}
+
---
+++ src/assignment2/ConsoleApp49/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+int c = 0;
+int s = 0;
+for (int i = 0; i < n; i++)
+{
+    int a, b;
+    var x = Console.ReadLine().Split(' ');
+    a = int.Parse(x[0]);
+    b = int.Parse(x[1]);
+
+    s = s - a;
+    s = s + b;
+    c = Math.Max(c, s);
+}
+Console.Write(c);
