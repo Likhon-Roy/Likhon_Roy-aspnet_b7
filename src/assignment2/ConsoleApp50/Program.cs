9fae9e3b88fbfbb874a80f78dd54fe14e3966720
LikhonR <likhon@learnteq.com>
2023-01-16 23:46:20 +0530

    one done

---
+++ src/assignment2/ConsoleApp50/Program.cs
@@ -0,0 +1,32 @@
+﻿int n, s, r, p = 0, z = 0;
+
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    var x = Console.ReadLine().Split(' ');
+
+    s = int.Parse(x[0]);
+    r = int.Parse(x[1]);
+
+    if (s > r)
+    {
+        p++;
+    }
+    else if (s < r)
+    {
+        z++;
+    }
+}
+if (p > z)
+{
+    Console.WriteLine("Mishka");
+}
+else if (z > p)
+{
+    Console.WriteLine("Chris");
+}
+else
+{
+    Console.WriteLine("Friendship is magic!^^");
+}
