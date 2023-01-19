d545c4ab58ff57da604eb74f2fd5a09b3e490dc1
LikhonR <likhon@learnteq.com>
2023-01-19 23:22:29 +0530

    1 done

--- src/assignment2/ConsoleApp51/Program.cs
+++ src/assignment2/ConsoleApp51/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 155 problem
+int count = 0;
+int a = int.Parse(Console.ReadLine());
+
+var arr = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int max = arr[0];
+int min = arr[0];
+
+for (int i = 0; i < a; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+        count++;
+    }
+    if (arr[i] < min)
+    {
+        min = arr[i];
+        count++;
+    }
+}
+Console.WriteLine(count);
