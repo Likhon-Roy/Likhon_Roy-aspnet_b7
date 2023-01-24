c601040fa9b39c7427a514d7eefce8a8f30f033f
LikhonR <likhon@learnteq.com>
2023-01-24 15:35:35 +0530

    sqs done

--- src/assignment7/SQSOperation/Program.cs
+++ src/assignment7/SQSOperation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.SQS.Model;
+using SQSOperation;
+
+var sqs = new SQSService();
+
+var sendMessageRequest = new SendMessageRequest
+{
+    DelaySeconds = 10,
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+        {
+            { "Title", new MessageAttributeValue { DataType = "String", StringValue = "The Whistler" } },
+            { "Author", new MessageAttributeValue { DataType = "String", StringValue = "John Grisham" } },
+            { "WeeksOn", new MessageAttributeValue { DataType = "Number", StringValue = "6" } }
+        },
+    MessageBody = "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+};
+
+await sqs.AddMssage(sendMessageRequest);
+await sqs.AddMssage("Add message only with body");
+
+await sqs.ReadTenMessage();
+
+// await sqs.DeleteReadMessages();
---
+++ src/assignment7/SQSOperation/SQSService.cs
@@ -0,0 +1,97 @@
+﻿using Amazon.SQS.Model;
+using Amazon.SQS;
+using Amazon;
+
+namespace SQSOperation
+{
+    public class SQSService
+    {
+
+        private const string QueueUrl = "https://sqs.us-east-1.amazonaws.com/847888492411/aspnetb7-likhon-SQS";
+        private readonly RegionEndpoint _bucketRegion = RegionEndpoint.USEast1;
+        private IAmazonSQS client;
+
+        public SQSService()
+        {
+            client = new AmazonSQSClient(_bucketRegion);
+        }
+
+        public async Task AddMssage(SendMessageRequest sendMessageRequest)
+        {
+            sendMessageRequest.QueueUrl = QueueUrl;
+
+            var response = await client.SendMessageAsync(sendMessageRequest);
+            Console.WriteLine("Sent a message with id : {0}", response.MessageId);
+        }
+
+        public async Task AddMssage(string messageBody)
+        {
+            var response = await client.SendMessageAsync(QueueUrl, messageBody);
+            Console.WriteLine("Sent a message with id : {0}", response.MessageId);
+        }
+
+        public async Task ReadTenMessage()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                var receiveMessageRequest = new ReceiveMessageRequest
+                {
+                    AttributeNames = new List<string>() { "All" },
+                    MaxNumberOfMessages = 1,
+                    MessageAttributeNames = { "All" },
+                    QueueUrl = QueueUrl,
+                    //VisibilityTimeout = (int)TimeSpan.FromMinutes(10).TotalSeconds,
+                    // WaitTimeSeconds = (int)TimeSpan.FromSeconds(5).TotalSeconds
+                };
+
+                var receiveMessageResponse = await client.ReceiveMessageAsync(receiveMessageRequest);
+
+                if (receiveMessageResponse.Messages.Count > 0)
+                    Console.WriteLine("Message Body: " + receiveMessageResponse.Messages[0].Body);
+            }
+        }
+
+        public async Task DeleteReadMessages()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                var receiveMessageRequest = new ReceiveMessageRequest
+                {
+                    AttributeNames = new List<string>() { "All" },
+                    MaxNumberOfMessages = 1,
+                    QueueUrl = QueueUrl,
+                    VisibilityTimeout = (int)TimeSpan.FromMinutes(10).TotalSeconds,
+                    WaitTimeSeconds = (int)TimeSpan.FromSeconds(5).TotalSeconds
+                };
+
+                var receiveMessageResponse = await client.ReceiveMessageAsync(receiveMessageRequest);
+
+                if (receiveMessageResponse.Messages.Count > 0)
+                {
+                    foreach (var message in receiveMessageResponse.Messages)
+                    {
+                        foreach (var x in message.Attributes)
+                        {
+                            if (x.Key == "ApproximateReceiveCount")
+                            {
+                                var totalRead = int.Parse(x.Value);
+                                if (totalRead > 1)
+                                {
+                                    Console.WriteLine("Deleted Message ID '" + message.MessageId + "', Body : " + message.Body);
+
+                                    var delRequest = new DeleteMessageRequest
+                                    {
+                                        QueueUrl = QueueUrl,
+                                        ReceiptHandle = message.ReceiptHandle
+                                    };
+
+                                    var delResponse = await client.DeleteMessageAsync(delRequest);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
